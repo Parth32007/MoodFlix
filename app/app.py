a03aea2450930f1ce2d9d380b1327275e44769b1
Parth Tambe <parthtambe327@gmail.com>
2026-06-02 16:31:00 +0530

    Basic frontend

--- app/app.py
+++ app/app.py
@@ -1,15 +1,22 @@
 import streamlit as st
+import pickle 
 
+movies=pickle.load(open('model/movies.pkl','rb'))
 st.title("MoodFlix")
 
 st.write("Emotion-based Movie Recommendation System")
 
-movies=["Avatar","Titanic","Interstellar","The dark knight"]
-selected_movie=st.selectbox("Select Movie",movies)
+movie_list=movies['title'].values
+selected_movie=st.selectbox("Select Movie",movie_list)
 
 moods=["Happy","Sad","Excited","Romantic","Motivational","Curious","Scared"]
 selected_mood=st.selectbox("Select Mood",moods)
 
+def recommend(movie):
+    return ["Movie 1","Movie 2","Movie 3","Movie 4","Movie 5"]
+
 if st.button("Recommend"):
-    st.write("Movie:",selected_movie)
-    st.write("Mood:",selected_mood)
+    recommendations=recommend(selected_movie)
+    st.subheader("Recommended Movies:")
+    for movie in recommendations:
+        st.write(movie)
