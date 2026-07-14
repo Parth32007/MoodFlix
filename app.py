e5b769ad786e7eb475f7b2b8e0563f5a02031fe1
Parth Tambe <parthtambe327@gmail.com>
2026-07-14 17:34:56 +0530

    Integrated AI recommendation engine with Flask

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from src.recommender import recommend_hybrid
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+
+    recommendations = []
+
+    if request.method == "POST":
+
+        movie = request.form["movie"]
+
+        mood = request.form["mood"]
+
+        recommendations = recommend_hybrid(movie, mood)
+
+        print(recommendations) 
+
+    return render_template("index.html", recommendations=recommendations)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
